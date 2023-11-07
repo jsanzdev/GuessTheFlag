c61b72327dbeb107f6143be8e397e1ca9d6a0b8c
Jesus Sanz <jesus@jsanz.dev>
2023-11-07 19:10:52 +0100

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jesus Sanz on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jesus Sanz on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
